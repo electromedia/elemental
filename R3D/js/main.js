a1443296613b5161456672384345cb1df62cc577
electromedia <mikelflores@EchoBLUEMBP.local>
2015-03-11 09:36:27 -0500

    Add folder
    Uploaded new test files

---
+++ R3D/js/main.js
@@ -0,0 +1,36 @@
+
+$(function() {
+var $menu = $('nav#menu'),
+$html = $('html, body');
+
+$menu.mmenu({
+dragOpen: true
+});
+
+$menu.find( 'li > a' ).on(
+'click',
+function( e )
+{
+var href = $(this).attr( 'href' );
+
+//	if the clicked link is linked to an anchor, scroll the page to that anchor
+if ( href.slice( 0, 1 ) == '#' )
+{
+$menu.one(
+'closed.mm',
+function()
+{
+setTimeout(
+function()
+{
+$html.animate({
+scrollTop: $( href ).offset().top
+});
+}, 10
+);
+}
+);
+}
+}
+);
+});
